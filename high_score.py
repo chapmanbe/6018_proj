738241902d994f95de546f765c9229a68672abe0
Annie Bowles <u1188019@utah.edu>
2017-12-19 05:39:35 +0000

    Updated space game and high score

--- high_score.py
+++ high_score.py
@@ -1,22 +1,20 @@
 import pickle
-import collections
 
-high_scores = [65536, 10000]
-#high_scores = high_scores.sort(reverse=True)
-#doesn’t work when you try to sort it
+high_scores = [320, 400]
 with open("highscores.pkl","wb") as out:
-	pickle.dump(high_scores, out)
+    pickle.dump(high_scores, out)
 
-new_score = 12000
+#new_score =
 
 with open("highscores.pkl","rb") as in_:
-	high_scores = pickle.load(in_)
+    high_scores = pickle.load(in_)
 if new_score not in high_scores:
-	high_scores.append(new_score)
+    high_scores.append(new_score)
+    high_scores.sort(reverse=True)
 with open("highscores.pkl","wb") as out:
-	pickle.dump(high_scores, out)
+    pickle.dump(high_scores, out)
 
 print("{{TITLE:^{PAGE_WIDTH}}}".format(PAGE_WIDTH=40).format(TITLE="HIGH SCORES"))
 print("-" * 40)
 for all_scores in high_scores:
-	print(all_scores)
+    print(all_scores)
